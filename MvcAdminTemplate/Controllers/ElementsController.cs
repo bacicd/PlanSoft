abc15fcd149fb145c64efbd25c5ee82709510d9d
N <N@BOX>
2017-02-05 14:27:55 -0600

    Elements and ElementsVariable UI mockups

---
+++ MvcAdminTemplate/Controllers/ElementsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAdminTemplate.Controllers
+{
+    public class ElementsController : Controller
+    {
+        //
+        // GET: /Elements/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
+
+
---
+++ MvcAdminTemplate/Controllers/ElementsVariablesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAdminTemplate.Controllers
+{
+    public class ElementsVariablesController : Controller
+    {
+        //
+        // GET: /ElementsVariables/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
