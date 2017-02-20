cebed18ac758c22116b99a90cfe063fb1efc7524
N <N@BOX>
2017-02-19 18:36:07 -0600

    CRUD v.1  for Elements and ElementsVariables views "I've never used jquery before!" Edition

--- MvcAdminTemplate/Controllers/ElementsController.cs
+++ MvcAdminTemplate/Controllers/ElementsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcAdminTemplate.Models;
 
 namespace MvcAdminTemplate.Controllers
 {
@@ -16,6 +17,69 @@ namespace MvcAdminTemplate.Controllers
             return View();
         }
 
+        public ActionResult LoadElements()
+        {
+            return Json(new
+            {
+                aaData = Elements.ElementsList.Select(x => new[] { x.ElementId, x.ElementName, x.DateSet.ToString(), x.CreatedBy})
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string EleID)
+        {
+            foreach (Elements i in Elements.ElementsList)
+            {
+                if (i.ElementId == EleID)
+                {
+                    Elements.ElementsList.Remove(i);
+                    return Json(new
+                    {
+                        aaData = Elements.ElementsList.Select(x => new[] { x.ElementId, x.ElementName, x.DateSet.ToString(), x.CreatedBy })
+                    }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            return Json(new
+            {
+                aaData = Elements.ElementsList.Select(x => new[] { x.ElementId, x.ElementName, x.DateSet.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Update(string EleID, string newID, string newName)
+        {
+            foreach (Elements i in Elements.ElementsList)
+            {
+                if (i.ElementId == EleID)
+                {
+                    i.ElementId = newID;
+                    i.ElementName = newName;
+                    i.DateSet = DateTime.Today;
+
+                    return Json(new
+                    {
+                        aaData = Elements.ElementsList.Select(x => new[] { x.ElementId, x.ElementName, x.DateSet.ToString(), x.CreatedBy })
+                    }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            return Json(new
+            {
+                aaData = Elements.ElementsList.Select(x => new[] { x.ElementId, x.ElementName, x.DateSet.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Add(string EleID, string EleName, string Creator)
+        {
+            Elements.ElementsList.Add(new Elements { ElementId = EleID, ElementName = EleName, DateSet = DateTime.Today, CreatedBy = Creator });
+
+            return Json(new
+            {
+                aaData = Elements.ElementsList.Select(x => new[] { x.ElementId, x.ElementName, x.DateSet.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
 
--- MvcAdminTemplate/Controllers/ElementsVariablesController.cs
+++ MvcAdminTemplate/Controllers/ElementsVariablesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcAdminTemplate.Models;
 
 namespace MvcAdminTemplate.Controllers
 {
@@ -14,6 +15,73 @@ namespace MvcAdminTemplate.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult LoadElements()
+        {
+            return Json(new
+            {
+                aaData = ElementsVariables.ElementsVariablesList.Select(x => new[] { x.ElementCode, x.ElementName, x.VariableCode, x.VariableName, x.VariableCID, x.DateSet.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string EleCode)
+        {
+            foreach (ElementsVariables i in ElementsVariables.ElementsVariablesList)
+            {
+                if (i.ElementCode == EleCode)
+                {
+                    ElementsVariables.ElementsVariablesList.Remove(i);
+                    return Json(new
+                    {
+                        aaData = ElementsVariables.ElementsVariablesList.Select(x => new[] { x.ElementCode, x.ElementName, x.VariableCode, x.VariableName, x.VariableCID, x.DateSet.ToString(), x.CreatedBy })
+                    }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            return Json(new
+            {
+                aaData = ElementsVariables.ElementsVariablesList.Select(x => new[] { x.ElementCode, x.ElementName, x.VariableCode, x.VariableName, x.VariableCID, x.DateSet.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Update(string varCode, string newEleCode, string newEleName, string newVarCID, string newVarName, string newVarCode)
+        {
+            foreach (ElementsVariables i in ElementsVariables.ElementsVariablesList)
+            {
+                if (i.ElementCode == varCode)
+                {
+                    i.ElementCode = newEleCode;
+                    i.ElementName = newEleName;
+                    i.VariableCID = newVarCID;
+                    i.VariableCode = newVarCode;
+                    i.VariableName = newVarName;
+                    i.DateSet = DateTime.Today;
+
+                    return Json(new
+                    {
+                        aaData = ElementsVariables.ElementsVariablesList.Select(x => new[] { x.ElementCode, x.ElementName, x.VariableCode, x.VariableName, x.VariableCID, x.DateSet.ToString(), x.CreatedBy })
+                    }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            return Json(new
+            {
+                aaData = ElementsVariables.ElementsVariablesList.Select(x => new[] { x.ElementCode, x.ElementName, x.VariableCode, x.VariableName, x.VariableCID, x.DateSet.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Add(string EleCode, string EleName, string varCode, string varName, string varCID, string Creator)
+        {
+            ElementsVariables.ElementsVariablesList.Add(new ElementsVariables { ElementCode = EleCode, ElementName = EleName, VariableCID = varCID, VariableCode = varCode, VariableName = varName, DateSet = DateTime.Today, CreatedBy = Creator });
+
+            return Json(new
+            {
+                aaData = ElementsVariables.ElementsVariablesList.Select(x => new[] { x.ElementCode, x.ElementName, x.VariableCode, x.VariableName, x.VariableCID, x.DateSet.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
         }
 
     }
