9a3b3db29827c645099992fe763beab71c8b1a6c
Joel <Joel@JLAPTOP>
2017-02-22 17:14:20 -0600

    Attributes and Attributes Variables pages updated with working models pulling from controllers.

--- MvcAdminTemplate/Controllers/AttributesController.cs
+++ MvcAdminTemplate/Controllers/AttributesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcAdminTemplate.Models;
 
 namespace MvcAdminTemplate.Controllers
 {
@@ -15,6 +16,71 @@ namespace MvcAdminTemplate.Controllers
         {
             return View();
         }
+        public ActionResult LoadAttributes()
+        {
+            return Json(new
+            {
+                aaData = Attributes.AttributesList.Select(x => new[] { x.ElementCode, x.ElementName, x.AttributeCode, x.AttributeName, x.DateSet.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
 
+        [HttpPost]
+        public ActionResult Delete(string EleCode)
+        {
+            foreach (Attributes i in Attributes.AttributesList)
+            {
+                if (i.ElementCode == EleCode)
+                {
+                    Attributes.AttributesList.Remove(i);
+                    return Json(new
+                    {
+                        aaData = Attributes.AttributesList.Select(x => new[] { x.ElementCode, x.ElementName, x.AttributeCode, x.AttributeName, x.DateSet.ToString(), x.CreatedBy })
+                    }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            return Json(new
+            {
+                aaData = Attributes.AttributesList.Select(x => new[] { x.ElementCode, x.ElementName, x.AttributeCode, x.AttributeName, x.DateSet.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Update(string EleCode, string NewEleCode, string NewEleName, string NewAttribCode, string NewAttribName)
+        {
+            foreach (Attributes i in Attributes.AttributesList)
+            {
+                if (i.ElementCode == EleCode)
+                {
+                    i.ElementCode = NewEleCode;
+                    i.ElementName = NewEleName;
+                    i.AttributeCode = NewAttribCode;
+                    i.AttributeName = NewAttribName;
+                    i.DateSet = DateTime.Today;
+
+                    return Json(new
+                    {
+                        aaData = Attributes.AttributesList.Select(x => new[] { x.ElementCode, x.ElementName, x.AttributeCode, x.AttributeName, x.DateSet.ToString(), x.CreatedBy })
+                    }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            return Json(new
+            {
+                aaData = Attributes.AttributesList.Select(x => new[] { x.ElementCode, x.ElementName, x.AttributeCode, x.AttributeName, x.DateSet.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Add(string EleCode, string EleName, string AttribCode, string AttribName, string Creator)
+        {
+            Attributes.AttributesList.Add(new Attributes { ElementCode = EleCode, ElementName = EleName, AttributeCode = AttribCode, AttributeName = AttribName, DateSet = DateTime.Today, CreatedBy = Creator });
+
+            return Json(new
+            {
+                aaData = Attributes.AttributesList.Select(x => new[] { x.ElementCode, x.ElementName, x.AttributeCode, x.AttributeName, x.DateSet.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
+
--- MvcAdminTemplate/Controllers/AttributesVariablesController.cs
+++ MvcAdminTemplate/Controllers/AttributesVariablesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcAdminTemplate.Models;
+
 
 namespace MvcAdminTemplate.Controllers
 {
@@ -14,6 +16,71 @@ namespace MvcAdminTemplate.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult LoadAttributes()
+        {
+            return Json(new
+            {
+                aaData = AttributesVariables.AttributesVariablesList.Select(x => new[] { x.AttributeCode, x.AttributeName, x.VariableCode, x.VariableName, x.DateSet.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string AttribCode)
+        {
+            foreach (AttributesVariables i in AttributesVariables.AttributesVariablesList)
+            {
+                if (i.AttributeCode == AttribCode)
+                {
+                    AttributesVariables.AttributesVariablesList.Remove(i);
+                    return Json(new
+                    {
+                        aaData = AttributesVariables.AttributesVariablesList.Select(x => new[] { x.AttributeCode, x.AttributeName, x.VariableCode, x.VariableName, x.DateSet.ToString(), x.CreatedBy })
+                    }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            return Json(new
+            {
+                aaData = AttributesVariables.AttributesVariablesList.Select(x => new[] { x.AttributeCode, x.AttributeName, x.VariableCode, x.VariableName, x.DateSet.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Update(string varCode, string NewAttribCode, string NewAttribName, string NewVarName, string NewVarCode)
+        {
+            foreach (AttributesVariables i in AttributesVariables.AttributesVariablesList)
+            {
+                if (i.AttributeCode == varCode)
+                {
+                    i.AttributeCode = NewAttribCode;
+                    i.AttributeName = NewAttribName;
+                    i.VariableCode = NewVarCode;
+                    i.VariableName = NewVarName;
+                    i.DateSet = DateTime.Today;
+
+                    return Json(new
+                    {
+                        aaData = AttributesVariables.AttributesVariablesList.Select(x => new[] { x.AttributeCode, x.AttributeName, x.VariableCode, x.VariableName, x.DateSet.ToString(), x.CreatedBy })
+                    }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            return Json(new
+            {
+                aaData = AttributesVariables.AttributesVariablesList.Select(x => new[] { x.AttributeCode, x.AttributeName, x.VariableCode, x.VariableName, x.DateSet.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Add(string AttribCode, string AttribName, string varCode, string varName, string Creator)
+        {
+            AttributesVariables.AttributesVariablesList.Add(new AttributesVariables { AttributeCode = AttribCode, AttributeName = AttribName, VariableCode = varCode, VariableName = varName, DateSet = DateTime.Today, CreatedBy = Creator });
+
+            return Json(new
+            {
+                aaData = AttributesVariables.AttributesVariablesList.Select(x => new[] { x.AttributeCode, x.AttributeName, x.VariableCode, x.VariableName, x.DateSet.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
         }
 
     }
