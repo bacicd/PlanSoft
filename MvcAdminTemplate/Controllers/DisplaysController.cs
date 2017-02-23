1e79f4c340c28aca5491ff69c28bd949eaacaedd
Joel <Joel@JLAPTOP>
2017-02-22 18:20:34 -0600

    Displays v1, test to get color picker working

---
+++ MvcAdminTemplate/Controllers/DisplaysController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAdminTemplate.Controllers
+{
+    public class DisplaysController : Controller
+    {
+        //
+        // GET: /Displays/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
