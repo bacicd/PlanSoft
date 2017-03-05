80e9937649a935efbb68604f06de3cc801c5dabc
Noah <N>
2017-03-04 20:25:35 -0600

    Partially done rules view. Edit button currently non function, add button in progress (currently adds dynamic inputs, does nothing with them)

--- MvcAdminTemplate/Controllers/RulesController.cs
+++ MvcAdminTemplate/Controllers/RulesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcAdminTemplate.Models;
 
 namespace MvcAdminTemplate.Controllers
 {
@@ -16,5 +17,76 @@ namespace MvcAdminTemplate.Controllers
             return View();
         }
 
+        public ActionResult LoadRuleNames()
+        {
+            return Json(new
+            {
+                aaData = Rules.RulesList.Select(x => new[] { x.RuleName})
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult LoadRules()
+        {
+            return Json(new
+            {
+                aaData = Rules.RulesList.Select(x => new[] { x.RuleCode, x.RuleName, x.MarkedField, x.RuleStatus, x.DateAdded.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string RCode)
+        {
+            foreach (Rules i in Rules.RulesList)
+            {
+                if (i.RuleCode == RCode)
+                {
+                    Rules.RulesList.Remove(i);
+                    return Json(new
+                    {
+                        aaData = Rules.RulesList.Select(x => new[] { x.RuleCode, x.RuleName, x.RuleStatus, x.DateAdded.ToString(), x.CreatedBy })
+                    }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            return Json(new
+            {
+                aaData = Rules.RulesList.Select(x => new[] { x.RuleCode, x.RuleName, x.RuleStatus, x.DateAdded.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Update(string RCode, string newCode, string newName)
+        {
+            foreach (Rules i in Rules.RulesList)
+            {
+                if (i.RuleCode == RCode)
+                {
+                    i.RuleCode = newCode;
+                    i.RuleName = newName;
+                    i.DateAdded = DateTime.Today;
+
+                    return Json(new
+                    {
+                        aaData = Rules.RulesList.Select(x => new[] { x.RuleCode, x.RuleName, x.RuleStatus, x.DateAdded.ToString(), x.CreatedBy })
+                    }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            return Json(new
+            {
+                aaData = Rules.RulesList.Select(x => new[] { x.RuleCode, x.RuleName, x.RuleStatus, x.DateAdded.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Add(string RCode, string RName, string RStatus, string Creator)
+        {
+            Rules.RulesList.Add(new Rules { RuleCode = RCode, RuleName = RName, RuleStatus = RStatus, DateAdded = DateTime.Today, CreatedBy = Creator });
+
+            return Json(new
+            {
+                aaData = Rules.RulesList.Select(x => new[] { x.RuleCode, x.RuleName, x.RuleStatus, x.DateAdded.ToString(), x.CreatedBy })
+            }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
---
+++ MvcAdminTemplate/Models/Rules.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcAdminTemplate.Models
+{
+    public class Rules
+    {
+        public string RuleCode { get; set; }
+        public string RuleName { get; set; }
+        public string MarkedField { get; set; }
+        public string RuleStatus { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        public static List<Rules> RulesList = new List<Rules>(new[]
+            {
+                new Rules { RuleCode = "100", RuleName = "Meaningful Measures", MarkedField = "Measure Meaningfulness", RuleStatus = "Active", CreatedBy = "Username" , DateAdded = DateTime.Today},
+                new Rules { RuleCode = "101", RuleName = "Plan Ownership Type", MarkedField = "101", RuleStatus = "", CreatedBy = "Username", DateAdded = DateTime.Today },
+            });
+    }
+}
