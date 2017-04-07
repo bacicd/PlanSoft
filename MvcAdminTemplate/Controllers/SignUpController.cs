56682f64f080d3a8f9514bb117dcbb15124c2526
bdbrown4 <bdbrown4@eagles.usi.edu>
2017-04-07 09:35:22 -0500

    redirects to ThankYou page after signup.

--- MvcAdminTemplate/Controllers/SignUpController.cs
+++ MvcAdminTemplate/Controllers/SignUpController.cs
@@ -7,44 +7,44 @@ using System.Web.Mvc;
 
 namespace MvcAdminTemplate.Controllers
 {
-    public class SignUpController : Controller
-    {
-        //
-        // GET: /SignUp/
+	public class SignUpController : Controller
+	{
+		//
+		// GET: /SignUp/
 
-        public ActionResult Index()
-        {
-            return View();
-        }
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-        public ActionResult emailSignUp(string fname, string lname, string title,
-                                        string companyname, string email, string phone,
-                                        string address, string city, string state, string zip)
-        {
-            MailMessage mail = new MailMessage();
+		public ActionResult emailSignUp(string fname, string lname, string title,
+										string companyname, string email, string phone,
+										string address, string city, string state, string zip)
+		{
+			MailMessage mail = new MailMessage();
 
-            mail.To.Add("dehassman@eagles.usi.edu");
+			mail.To.Add("bdbrown4@eagles.usi.edu");
 
-            mail.IsBodyHtml = true;
+			mail.IsBodyHtml = true;
 
-            SmtpClient smtpClient = new SmtpClient();
+			SmtpClient smtpClient = new SmtpClient();
 
-            mail.Subject = "Sign Up from HResonance";
-            mail.Body = "<div style=\"font: 15px Calibri, arial;\">";
-            mail.Body += "<p>" + fname + "</p>";
-            mail.Body += "<p>" + lname + "</p>";
-            mail.Body += "<p>" + title + "</p>";
-            mail.Body += "<p>" + companyname + "</p>";
-            mail.Body += "<p>" + email + "</p>";
-            mail.Body += "<p>" + phone + "</p>";
-            mail.Body += "<p>" + address + "</p>";
-            mail.Body += "<p>" + city + "</p>";
-            mail.Body += "<p>" + state + "</p>";
-            mail.Body += "<p>" + zip + "</p>";
-            smtpClient.Send(mail);
+			mail.Subject = "Sign Up from HResonance";
+			mail.Body = "<div style=\"font: 15px Calibri, arial;\">";
+			mail.Body += "<p>" + fname + "</p>";
+			mail.Body += "<p>" + lname + "</p>";
+			mail.Body += "<p>" + title + "</p>";
+			mail.Body += "<p>" + companyname + "</p>";
+			mail.Body += "<p>" + email + "</p>";
+			mail.Body += "<p>" + phone + "</p>";
+			mail.Body += "<p>" + address + "</p>";
+			mail.Body += "<p>" + city + "</p>";
+			mail.Body += "<p>" + state + "</p>";
+			mail.Body += "<p>" + zip + "</p>";
+			smtpClient.Send(mail);
 
-            return View();
-        }
+			return Json(new { message = "success!" }, JsonRequestBehavior.AllowGet);
+		}
 
-    }
+	}
 }
