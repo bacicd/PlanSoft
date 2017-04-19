9298b6737006e48cccb8411107b45c01c4df4c06
bdbrown <bdbrown4@eagles.usi.edu>
2017-04-18 22:55:10 -0500

    Made the First and Last name appear by logout and the Created By field in the modals for the datatables populates a label with the users information too

--- MvcAdminTemplate/Controllers/HomeController.cs
+++ MvcAdminTemplate/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcAdminTemplate.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -26,6 +27,40 @@ namespace MvcAdminTemplate.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public JsonResult firstNameLastName(string fname, string lname)
+        {
+            var accountContext = new DBModelEntities();
+            IList<Account> accountlist = accountContext.Accounts.ToList();
+            Account acc = new Account();
+
+            var userNameList = accountlist.Select(x => x.Username);
+
+            foreach (string userName in userNameList)
+            {
+                var fnameList = accountlist.Where(x => x.Username.Equals(userName)).Select(x => x.First);
+                var lnameList = accountlist.Where(x => x.Username.Equals(userName)).Select(x => x.Last);
+                foreach (string obj in fnameList)
+                {
+                    if (userName.Equals(User.Identity.Name))
+                    {
+                        fname = obj;
+                    }
+                }
+                foreach (string obj in lnameList)
+                {
+                    if (userName.Equals(User.Identity.Name))
+                    {
+                        lname = obj;
+                    }
+                }
+            }
+            
+            return Json(new
+            {
+                fname,lname
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
